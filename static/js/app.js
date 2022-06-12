0d6c25960c715af95d525309ae7158b667e8cef7
Tim Nilsen <nilsentim.e@gmail.com>
2022-06-11 19:36:43 -0700

    finished 11.5.2

--- static/js/app.js
+++ static/js/app.js
@@ -1,38 +1,15 @@
-// import the data from data.js
-const tableData = data;
-
-// reference the HTML table using d3
-var tbody = d3.select('tbody');
-
-// simple JacaScript console.log statement
-function printHello() {
-    console.log('Hello there!');
-}
-
-// takes two numbers and adds them
-function addition(a, b) {
-    return a + b;
-}
-
-// functions can call other functions 
-function doubleAddition(c, d) {
-    var total = addition(c, d) * 2;
-    return total;
-}
-
-// example of an arrow function for printHello()
-printHello = () => 'Hello There'; 
-
-// example of an arrow function for addition()
-addition = (a, b) => a + b;
-
-// example of an arrow funciton for doubleAddition
-doubleAddition = (c, d) => addition(c, d) * 2; 
-
-let friends = ['Sarah', 'Greg', 'Cindy', 'Jeff']; 
-
-function listLoop(userList) {
-    for (var i = 0; i < userList.length; i++) {
-        console.log(userList[i]);
-    }
+function buildTable(data) {
+    // clear out any existing data in the table
+    tbody.html('');
+    // loop through each object in the data
+    // and append a row and cells for each value in the row
+    data.forEach((dataRow) => {
+        // append a row to the table body
+        let row = tbody.append('tr');
+        // loop through each field in the dataRow and add each value as a table cell (td)
+        Object.values(dataRow).forEach((val) => {
+            let cell = row.append('td');
+            cell.text(val);
+        });
+    });
 }
