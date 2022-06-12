a5fd68d81eabe5061fc6552bd66ca489302db919
Tim Nilsen <nilsentim.e@gmail.com>
2022-06-11 19:53:44 -0700

    finished 11.5.4

--- static/js/app.js
+++ static/js/app.js
@@ -1,3 +1,9 @@
+// import the data from data.js
+const tableData = data;
+
+// reference the HTML table using d3
+var tbody = d3.select('tbody');
+
 function buildTable(data) {
     // clear out any existing data in the table
     tbody.html('');
@@ -12,4 +18,23 @@ function buildTable(data) {
             cell.text(val);
         });
     });
-}
+};
+
+function handleClick() {
+    // grab the datetime value from the filter
+    let date = d3.select('#datetime').property('value');
+    let filteredData = tableData;
+    // check to see if a date was entered and filter the data using that date
+    if (date) {
+        filteredData = filteredData.filter(row => row.datetime === date);
+    };
+    // rebuild the table using the filtered data
+    // @NOTE: if no date was entered then filteredData will simply be the original tableData
+    buildTable(filteredData);
+};
+
+// attach an event to listen for the form button
+d3.selectAll('#filter-btn').on('click', handleClick);
+
+// build the table when the page loads
+buildTable(tableData);
